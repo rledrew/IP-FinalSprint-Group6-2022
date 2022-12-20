358fec04e19634dcb18d6c9e994902b3d8d6df47
ShovelKnight11 <riley.ledrew@keyin.com>
2022-12-20 10:59:51 -0330

    Finished employee menu and added new write functions for files.

--- sprint-final-s1.py
+++ sprint-final-s1.py
@@ -1,3 +1,10 @@
+# Importing Libraries
+from functions import replace_line
+from functions import namevalidate
+import datetime
+import datetime as dtime
+# Setting Date variables
+tday = dtime.date.today()
 # Opening files & setting globals
 global transnum,nextdrivernum,MONTHSTANDFEE,DAILYFEE,WEEKLYFEE,HST_RATE
 defaultread = open('Defaults.dat')
@@ -9,22 +16,48 @@ MONTHSTANDFEE = defaultlines[2]
 DAILYFEE = defaultlines[3]
 WEEKLYFEE = defaultlines[4]
 HST_RATE = defaultlines[5]
+PreviousDate = defaultlines[6]
+if tday != PreviousDate:
+    replace_line('Defaults.dat', 6, f'{tday}')
+revread = open('revenue.dat', 'a')
+if tday.day == 1:
+    revread.write(f'{transnum}')
 
 # Functions
-def replace_line(file_name, line_num, text):
-    lines = open(file_name, 'r').readlines()
-    lines[line_num] = text
-    out = open(file_name, 'w')
-    out.writelines(lines)
-    out.close()
+
 def NewEmpMenu():
+    global nextdrivernum
+    empfileread = open('employees.dat', 'a')
+    driver_num = nextdrivernum
+    nextdrivernum += 1
+    replace_line('Defaults.dat', 1, f'{nextdrivernum}\n')
     while True:
-        global nextdrivernum
-        empfileread = open('employees.dat', 'a')
-        driver_num = nextdrivernum
-        nextdrivernum += 1
-        replace_line('Defaults.dat', 1, f'{nextdrivernum}\n')
-        break
+        firstname = input('Enter employee first name here: ')
+        if firstname.isalpha() == False:
+            print('Please input a valid name')
+        else:
+            break
+    while True:
+        lastname = input('Enter employee last name here: ')
+        if lastname.isalpha() == False:
+            print('Please input a valid name')
+        else:
+            break
+
+    empaddress = input('Enter employee address here(## Street, City): ')
+    while True:
+        empphone = input('Enter employee phone number here(###-###-####): ')
+        empphone = empphone.replace(' ', '')
+        empphone = empphone.replace('-', '')
+        if empphone.isnumeric() == False:
+            print('Phone number must contain only numbers and dashes')
+        else:
+            break
+    empphonedsp = f'{empphone[0]}{empphone[1]}{empphone[2]}-{empphone[3]}{empphone[4]}{empphone[5]}' \
+                      f'-{empphone[6]}{empphone[7]}{empphone[8]}{empphone[9]}'
+
+
+
 # def RevenueMenu():
 
 # Menu setup
@@ -71,3 +104,4 @@ def menu():
         else:
             print('Please enter a valid choice')
 '''
+NewEmpMenu()
